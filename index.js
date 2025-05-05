9c360eb9fbd0b9cc36770b5af5185f0988044593
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2025-05-05 12:25:19 +0530

    step -3 Added Routers

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const express = require("express");
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
 
 const app = express();
 
-app.post("/user/signup", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
-});
-app.post("/user/signin", function (req, res) {
-  res.json({
-    message: "signin endpoint",
-  });
-});
-app.get("/user/course", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
-});
-app.post("/course/purchase", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
-});
-app.post("/course/preview", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
-});
+app.use("/user", userRouter);
+app.use("/course", courseRouter);
+
+app.listen(3000);
