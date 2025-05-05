a379348bfa2896e405aec53de7b891dbf01061b0
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2025-05-05 16:15:09 +0530

    step -7 added a scripts

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,32 @@
 const { Router } = require("express");
-const { adminModel } = reqiure("../db");
 const adminRouter = Router();
+const { adminModel } = reqiure("../db");
 
-app.post("/signup", function (req, res) {
+adminRouter.post("/signup", function (req, res) {
   res.json({
     message: "signup endpoint",
   });
 });
 
-app.post("/signin", function (req, res) {
+adminRouter.post("/signin", function (req, res) {
   res.json({
     message: "signin endpoint",
   });
 });
 
-app.post("/course", function (req, res) {
+adminRouter.post("/course", function (req, res) {
   res.json({
     message: "signup endpoint",
   });
 });
 
-app.put("/course", function (req, res) {
+adminRouter.put("/course", function (req, res) {
   res.json({
     message: "signup endpoint",
   });
 });
 
-app.get("/course/bulk", function (req, res) {
+adminRouter.get("/course/bulk", function (req, res) {
   res.json({
     message: "signup endpoint",
   });
